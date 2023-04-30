2de03854cd30861b36c8c2bd331e54287500690a
lab1 <zhanerketkva7@gmail.com>
2023-04-30 16:47:11 +0600

    implemented peek method for MyArrayListQueue.java

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -15,4 +15,10 @@ public class MyArrayListQueue<E> {
         }
         return list.remove(0);
     }
+    public E peek() { // method is to return the front element of the queue without removing it
+        if (list.isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return list.get(0);
+    }
 }
