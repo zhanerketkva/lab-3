c94e5c3297872bfb974a26e0d2cb8916371e2da7
lab1 <zhanerketkva7@gmail.com>
2023-04-30 16:23:02 +0600

    MyArrayListQueue.java

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,2 +1,10 @@
-public class MyArrayListQueue {
+import java.util.NoSuchElementException; //necessary exception
+
+public class MyArrayListQueue<E> {
+    private MyArrayList<E> list;
+
+    public MyArrayListQueue() {
+        list = new MyArrayList<>();
+    }
+
 }
