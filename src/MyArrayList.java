a9814d59095164eff0782b2ef261a10294b8c11c
lab1 <zhanerketkva7@gmail.com>
2023-04-30 16:12:49 +0600

    MyArrayList.java

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,65 @@
-public class MyArrayList {
+import java.util.*;
+public class MyArrayList<E> implements List<E> {
+    private Object[] elements;
+    private int size;
+    public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public boolean add(E element) {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+        elements[size++] = element;
+        return true;
+    }
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (E) elements[index];
+    }
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elements[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            public E next() {
+                return (E) elements[currentIndex++];
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+        };
+    }
+
+
 }
