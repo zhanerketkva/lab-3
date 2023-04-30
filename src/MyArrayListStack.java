35b60c45f3a50640593bf39ed9c0d97f9a2d20e8
lab1 <zhanerketkva7@gmail.com>
2023-04-30 16:21:09 +0600

    MyArrayListStack.java

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,2 +1,10 @@
-public class MyArrayListStack {
+import java.util.EmptyStackException; //necessary exception
+import java.util.NoSuchElementException;
+
+public class MyArrayListStack<E> {
+    private MyArrayList<E> list;
+
+    public MyArrayListStack() {
+        list = new MyArrayList<>();
+    }
 }
