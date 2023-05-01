e93cae89c9d171a5cbb2809cfd5e6993f5843e25
lab1 <zhanerketkva7@gmail.com>
2023-05-01 21:58:23 +0600

    main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.NoSuchElementException;
+
+public class main {
+    public static void main(String[] args) {
+        MyArrayListQueue<String> queue = new MyArrayListQueue<>();
+        MyArrayListStack<Integer> stack = new MyArrayListStack<>();
+    }
+}
