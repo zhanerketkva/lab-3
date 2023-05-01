825242734c580a17871b6522aa6c2e25b10dc383
lab1 <zhanerketkva7@gmail.com>
2023-05-01 22:26:23 +0600

    comments for MyArrayListStack

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -10,18 +10,21 @@ public class MyArrayListStack<E> {
     public void push(E element) {
         list.add(element);
     }
+    /**add element**/
     public E pop() {
         if (list.isEmpty()) {
             throw new NoSuchElementException("Stack is empty");
         }
         return list.remove(list.size() - 1);
     }
+    /**remove the top element**/
     public E peek() {
         if (list.isEmpty()) {
             throw new EmptyStackException();
         }
         return list.get(list.size() - 1);
     }
+
     public boolean isEmpty() {
         return list.isEmpty();
     }
